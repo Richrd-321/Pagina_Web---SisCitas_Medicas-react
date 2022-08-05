d645fbb22df42d7c33cffba5785766ddd392fb22
Diego Ortiz Carmelino <dortiz@SEDACUSCO.COM>
2022-08-05 12:41:08 -0500

    1er Commit dede la Oficina

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header"
+import Formulario from "./components/Formulario"
+import Listado_Citas from "./components/Listado_Citas"
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Formulario />
+
+      <Listado_Citas />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Listado_Citas.jsx
@@ -0,0 +1,11 @@
+//import React from 'react'
+
+function Listado_Citas() {
+  return (
+    <div>
+        <h2>Listado Pacientes</h2>
+    </div>
+  )
+}
+
+export default Listado_Citas
